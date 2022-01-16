a8b1ca1f1ac9aa44769cec8d39e9d23c98692bdf
Fox <31753655+xXxsomebodyoncetoldmexXx@users.noreply.github.com>
2022-01-16 08:20:28 +0700

    Fix empty request and generalized the library

--- SimpleHTTPServer/Server.py
+++ SimpleHTTPServer/Server.py
@@ -33,54 +33,6 @@ _hex_to_bytes = {(a + b).encode(): bytes.fromhex(a + b)
                  for a in _hex_digits for b in _hex_digits}
 
 
-ERROR_BODY = """\
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-        "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-        <title>Error response</title>
-    </head>
-    <body>
-        <h1>Error response</h1>
-        <p>Error code: {code}</p>
-        <p>Message: {message}.</p>
-        <p>Error code explanation: {code} - {explain}.</p>
-    </body>
-</html>
-"""
-
-OK_BODY = """\
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-        "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js"
-          integrity="sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        ></script>
-        <script type="text/javascript" defer src="/main.js"></script>
-        <title>Directory listing for {path}</title>
-    </head>
-    <body>
-        <h1>Directory listing for {path}</h1>
-        {body}
-        <hr>
-        <h1>Upload file to {path}</h1>
-        <input type="file" name="upload" id="uploadFile" />
-        <button type="button" onclick="saveFile()">Send</button>
-    </body>
-</html>
-"""
-
-
-def _fix_eol(msg):
-  return re.sub(r'(?:\r\n|\n|\r(?!\n))', CRLF, msg)
-
-
 def unquote_to_bytes(s):
   if not s:
     return b''
@@ -126,17 +78,19 @@ class SimpleServer:
 
   support_request_type = ("GET", "POST", "HEAD")
 
-  def __init__(self, host="127.0.0.1", port=8080, queue_size=5, poll_timeout=1.0):
+  def __init__(self, host="127.0.0.1", port=8080, queue_size=5, poll_timeout=0.1):
     self.host = host
     self.port = port
     self.request_queue_size = queue_size
     self.poll_timeout = poll_timeout
-    self.__header_buffer = dict()
-    self.resp_code = None
-    self.header = dict()
-    self.body = None
-    self.client_sock = None
-    self.client_stream = None
+    self.__header_buffer = {}
+    self.header = {}
+    self.__init_resp = b""
+    self.body = b""
+    self.c_sock = None
+    self.c_stream = None
+    self.middlewares = []
+    self.routes = []
     self._bind()
 
   def _bind(self):
@@ -154,46 +108,38 @@ class SimpleServer:
     return email.utils.formatdate(t, usegmt=True)
 
   def _get_resp(self):
-    resp = list()
+    resp = []
+    blank_count = 0
     while True:
-      line = self.client_stream.readline(_MAX_LINE).strip(b" \r\t\n")
-      if not line:
-        break
+      line = self.c_stream.readline(_MAX_LINE).strip(b" \r\t\n")
       logging.debug(f"recv {line}")
+      if not line:
+        blank_count += 1
+      elif line and blank_count:
+        blank_count = 0
+      if blank_count == 2:
+        break
       resp.append(line)
-    # Process
     return b'\n'.join(resp)
 
-  def _do_resp(self, body=b""):
-    self._set_header("Content-Length", str(len(body)))
+  def _do_resp(self):
+    if isinstance(self.body, str):
+      self.body = self.body.encode()
+    self.set_header("Content-Length", str(len(self.body)))
 
-    data = self.resp_code
+    data = self.__init_resp
     data += self.__translate_header()
-    data += body
-    if not data.endswith(bCRLF):
+    data += self.body
+    while not data.endswith(bCRLF + bCRLF):
       data += bCRLF
-    self.client_sock.sendall(data)
+    self.c_sock.sendall(data)
     logging.debug(b"send %s" % data)
-
-  def _clean_up(self):
-    self.resp_code = None
-    self.__header_buffer = {}
-    self.header = {}
-    self.body = None
-    self.client_stream.close()
-    try:
-      self.client_sock.shutdown(socket.SHUT_RDWR)
-    except OSError:
-      pass
-    self.client_sock.close()
-    self.client_stream = None
-    self.client_sock = None
 
   def _process_params(self, uri):
     path, query = uri.split('?', 1)
     if not query[1]:
-      return (path, {})
-    query = query.split('#', 1)[0]
+      return (path, {}, '')
+    query, fragment = query.split('#', 1)[0]
     params = dict()
     if query:
       for name_value in query.split('&'):
@@ -205,10 +151,10 @@ class SimpleServer:
             value = nv[1].replace('+', ' ')
             value = unquote(value)
             params[name] = value
-    return (path, params)
+    return (path, params, fragment)
 
-  def _process_header(self):
-    data = [s.decode() for s in self._get_resp().split(b'\n')]
+  def _process_header(self, raw):
+    data = [s.decode() for s in raw.split(b'\n')]
     for line in data:
       mo = _HeaderRe.match(line)
       if mo:
@@ -220,26 +166,29 @@ class SimpleServer:
           self.header["URI"] = mo.group("URI")
           self.header["HTTP-Version"] = mo.group("HTTPVersion")
           if "?" in self.header["URI"]:
-            self.header["File-Path"], self.header["Params"] = self._process_params(
+            self.header["File-Path"], self.header["Params"], self.header["Fragment"] = self._process_params(
                 self.header["URI"])
           else:
             self.header["File-Path"] = self.header["URI"]
 
-  def _process_body(self):
-    data = self._get_resp()
-    if self.header["Content-Type"] == "application/json":
-      data = json.loads(data)
-      data["data"] = data["data"].split(",", 1)[1]
-      print(data["data"])
-      data["data"] = b64decode(data["data"])
-    return data
+  def _process_body(self, raw):
+    if raw:
+      return raw[0].decode()
+    return ''
 
-  def _set_resp_code(self, code):
-    self.resp_code = f"HTTP/1.0 {code} {self.responses[code][0]}{CRLF}".encode(
-    )
-
-  def _set_header(self, key, value):
-    self.__header_buffer[key] = value
+  def __clean_up(self):
+    self.__init_resp = b""
+    self.__header_buffer = {}
+    self.header = {}
+    self.body = b""
+    self.c_stream.close()
+    try:
+      self.c_sock.shutdown(socket.SHUT_RDWR)
+    except OSError:
+      pass
+    self.c_sock.close()
+    self.c_stream = None
+    self.c_sock = None
 
   def __translate_header(self):
     h = list()
@@ -255,47 +204,60 @@ class SimpleServer:
       pass
     self.sock.close()
 
-  def err(self, code):
-    try:
-      shortmsg, longmsg = self.responses[code]
-    except KeyError:
-      shortmsg, longmsg = '???', '???'
+  def middleware(self, func):
+    self.middlewares.append(func)
 
-    self._set_resp_code(code)
-    self._set_header("Connection", "close")
-    body = ERROR_BODY.format(**{
-        "code": code,
-        "message": html.escape(shortmsg, quote=False),
-        "explain": html.escape(longmsg, quote=False)
-    })
-    body = _fix_eol(body).encode('utf-8', 'replace')
-    self._set_header("Content-Type", "text/html;charset=utf-8")
-    if self.header["Request-Type"] != "HEAD":
-      return self._do_resp(body)
-    self._do_resp()
+  def route(self, route, methods=("GET")):
+    if isinstance(route, str):
+      # Make raw string
+      route = route.encode("unicode_escape").decode()
+    route = re.compile(r"^%s$" % route)
+
+    def decorator(func, *args, **kwargs):
+      self.routes.append({
+          "route": route,
+          "methods": methods,
+          "func": func,
+          "argc": len(args) + len(kwargs)
+      })
+    return decorator
+
+  def set_resp_code(self, code):
+    self.__init_resp = f"HTTP/1.0 {code} {self.responses[code][0]}{CRLF}".encode(
+    )
+
+  def set_header(self, key, value):
+    self.__header_buffer[key] = value
 
   def redirect(self, path):
-    self._set_resp_code(HTTPStatus.MOVED_PERMANENTLY)
-    self._set_header("Location", path)
-    self._do_resp()
+    self.set_resp_code(HTTPStatus.MOVED_PERMANENTLY)
+    self.set_header("Location", path)
 
   def hander_req(self):
     try:
-      self.client_sock, addr = self.sock.accept()
-      self.client_stream = self.client_sock.makefile("rb")
+      self.c_sock, addr = self.sock.accept()
+      self.c_stream = self.c_sock.makefile("rb")
       logging.debug(f"New connection from {addr}")
-      self._process_header()
-      logging.debug(self.header)
-      logging.info(
-          f"{addr[0]} - {self.header['Request-Type']} - {self.header['URI']}")
-      if self.header["Request-Type"] in ("POST"):
-        self.body = self._process_body()
-      if self.header.get("Connection", "") != "close":
-        self.hander_resp()
-    except Exception:
-      # FIXME: Some time it die, no idea why????????????????????????????
-      # the client send no content and/or browser just end the connection
-      pass
+      raw_header, *raw_body = self._get_resp().split(b"\n\n")
+      if len(raw_header):
+        self._process_header(raw_header)
+        self.body = self._process_body(raw_body)
+        logging.debug(self.header)
+        logging.info(
+            f"{addr[0]} - {self.header['Request-Type']} - {self.header['URI']}")
+        if self.header["Request-Type"] in ("POST", "PUT"):
+          self.body = self._process_body()
+
+        # Run middlewares
+        for func in self.middlewares:
+          cont = func(self)
+          if not cont:
+            return self._do_resp()
+
+        if self.header.get("Connection", "") != "close":
+          self.hander_resp()
+    finally:
+      self.__clean_up()
 
   def check_parent(self, path):
     path = path.lstrip('/').split('/')
@@ -305,81 +267,35 @@ class SimpleServer:
       parent.append(part if part.exists() else None)
     return (path, parent)
 
-  def guess_type(self, path):
+  def guess_file_type(self, path):
     ext = path.suffix
     if ext in self.extensions_map:
       return self.extensions_map[ext]
-    guess, _ = mimetypes.guess_extension(path)
+    guess, _ = mimetypes.guess_type(path)
     if guess:
       return guess
     return 'application/octet-stream'
 
-  def list_dir(self, path):
-    self._set_resp_code(HTTPStatus.OK)
-    body = []
-    lnk = '<li><a href="{name}">{name}</a></li>'
-    params = json.dumps(self.header.get("Params", ""), ensure_ascii=False)
-    body.append(f"<h2>Parameters include in the request: {params}</h2>")
-    body.append("<hr>")
-    body.append("<ur>")
-    if path.name:
-      body.append(lnk.format(**{
-          "name": ".."
-      }))
-    for directory in path.iterdir():
-      name = directory.name if directory.is_file() else f"{directory.name}/"
-      body.append(lnk.format(**{
-          "name": name
-      }))
-    body.append("</ur>")
-    body = OK_BODY.format(**{
-        "path": self.header["File-Path"],
-        "body": '\n'.join(body)
-    }).encode('utf-8', 'replace')
-    self._do_resp(body)
-
-  def send_file(self, file):
-    self._set_resp_code(HTTPStatus.OK)
-    # Only nessensary for live reading
-    # self._set_header("Content-type", self.guess_type(file))
-
-    # Instally download
-    self._set_header("Content-Type", "application/octet-stream")
-
-    with open(file, "rb") as f:
-      content = f.read()
-    self._do_resp(content)
-
-  def save_file(self, path):
-    # There is a security vulnerability here, but it's just a challenge for web server so i'm just gonna ignore it ;)
-    with open(Path(path, self.body["name"]), "wb") as f:
-      f.write(self.body["data"])
-
   def hander_resp(self):
-    self._set_header("Server", self._server_version())
-    self._set_header("Date", self._get_time())
-    path, parents = self.check_parent(self.header["File-Path"])
-    if None in parents:
-      return self.err(HTTPStatus.NOT_FOUND)
-    if self.header["Request-Type"] not in self.support_request_type:
-      return self.err(HTTPStatus.NOT_IMPLEMENTED)
-    elif self.header["Request-Type"] == "GET":
-      # do_GET
-      if path.is_dir():
-        if not self.header["File-Path"].endswith("/"):
-          return self.redirect(self.header["File-Path"] + "/")
-        return self.list_dir(path)
-      if not path.exists() and path.name:
-        return self.err(HTTPStatus.NOT_FOUND)
-      self.send_file(path)
-    else:
-      # do_POST
-      if not path.is_dir():
-        return self.err(HTTPStatus.NOT_ACCEPTABLE)
-      self.save_file(path)
-      # Delay
-      time.sleep(1)
-      self.redirect(self.header["File-Path"])
+    self.set_header("Server", self._server_version())
+    self.set_header("Date", self._get_time())
+    self.set_header("Connection", "close")
+
+    # routing
+    for route in self.routes:
+      logging.debug(
+          f"Try to match {route['route']} to path {self.header['File-Path']}")
+      mo = route["route"].match(self.header["File-Path"])
+      if mo and self.header["Request-Type"] in route["methods"]:
+        if route["argc"] > 1:
+          route["func"](self, mo.groups())
+        else:
+          route["func"](self)
+        return self._do_resp()
+
+    # default route
+    self.set_resp_code(HTTPStatus.NOT_FOUND)
+    self._do_resp()
 
   def serve_forever(self):
     with SelectSelector() as selector:
@@ -389,7 +305,6 @@ class SimpleServer:
           ready = selector.select(self.poll_timeout)
           if ready:
             self.hander_req()
-            self._clean_up()
       finally:
         self.__exit__()
 
@@ -400,9 +315,6 @@ class SimpleServer:
     except KeyboardInterrupt:
       print("\nKeyboard interrupt received, exiting.")
       sys.exit(0)
-    # except Exception:
-    #   logging.CRITICAL(f"Server reboot")
-    #   time.sleep(3)
 
   def fileno(self):
     # For selector
@@ -410,7 +322,29 @@ class SimpleServer:
 
 
 def main():
-  SimpleServer(host="0.0.0.0").start()
+  app = SimpleServer(host="0.0.0.0")
+
+  @app.middleware
+  def serve_script_file(request):
+    file = Path(request.header["File-Path"].lstrip("/"))
+    if file.exists() and file.is_file() and file.suffix == ".js":
+      request.set_resp_code(HTTPStatus.OK)
+      request.set_header("Content-type", request.guess_file_type(file))
+      with open(file, "rb") as f:
+        request.body = f.read()
+      return False
+    return True
+
+  @app.route("/")
+  def test(request):
+    request.set_resp_code(HTTPStatus.OK)
+    request.body = "<h1> test </h1>"
+
+  @app.route("/path1")
+  def test2(request):
+    request.redirect("/")
+
+  app.start()
 
 
 if __name__ == "__main__":
