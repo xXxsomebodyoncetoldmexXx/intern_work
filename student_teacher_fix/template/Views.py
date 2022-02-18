0c33c7269b8cf834a34d1c6895c7e3fb20da1449
xXxsomebodyoncetoldmexXx <csgo0507@gmail.com>
2022-02-18 16:40:55 +0700

    Fix xss with html escapse

--- student_teacher_fix/template/Views.py
+++ student_teacher_fix/template/Views.py
@@ -1,4 +1,5 @@
 import re
+import html
 from pathlib import Path
 from .index import parse_options as index_parse
 from .user import parse_options as user_parse
@@ -23,6 +24,18 @@ def _fix_eol(msg):
     return re.sub(r"(?:\r\n|\n|\r(?!\n))", CRLF, msg)
 
 
+def html_safe_escape(data):
+    if isinstance(data, str):
+        data = html.escape(data)
+    elif isinstance(data, list):
+        for idx, value in enumerate(data):
+            data[idx] = html_safe_escape(value)
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            data[key] = html_safe_escape(value)
+    return data
+
+
 def read_file(path):
     data = ""
     with open(path, "r") as f:
@@ -40,6 +53,9 @@ def read_bytes(path):
 def render(path, args):
     path = Path(PREFIX + path)
     if path.exists():
+        print("bef", args)
+        args = html_safe_escape(args)
+        print("aft", args)
         if parser_list.get(path.name, ""):
             args = parser_list[path.name](args)
         data = read_file(path)
